282799b3fdc374e03eed73d83a076d5be71f2074
Marius Gedminas <marius@gedmin.as>
2014-11-27 14:40:45 +0200

    Claim Python 3.4 support

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
